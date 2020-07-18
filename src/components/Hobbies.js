8e77120bc9c330371035e82416fd7dc45aa5c326
David Padilla <dpadman2@gmail.com>
2020-07-18 02:04:31 -0500

    update home

---
+++ src/components/Hobbies.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/Hobbies.css'
+
+class Hobbies extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Hobbies;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
                 <h1 className="header">David Padilla</h1>
                 <div id="content">
                     <h3>Full Stack Developer | Father | Husband | Gamer</h3>
-                    <h5>Welcome to my portfolio. <a href="/About"> Learn more about me!</a></h5>
+                    <h5>Welcome to my portfolio. </h5>
                 </div>
             </div>
         )
